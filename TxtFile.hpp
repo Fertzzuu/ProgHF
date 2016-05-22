34f416fa6ba2c64095b93d93b35d25ddb6caf337
Marci <marcell.megyeri@mistorygame.com>
2016-05-22 16:08:20 +0200

    esrfer

---
+++ TxtFile.hpp
@@ -0,0 +1,259 @@
+//
+// Created by mmarci on 2016.05.15..
+//
+
+#ifndef PROGHF_TXTFILE_H
+#define PROGHF_TXTFILE_H
+
+
+#include <cstdio>
+#include <cstdlib>
+#include "String.hpp"
+#include "Vector.hpp"
+#include "Algorithms.h"
+
+
+
+using namespace std;
+
+class TxtFile{
+    Vector<String> lines;
+
+public:
+
+    typedef String* Iterator;
+
+    TxtFile(){ lines = Vector<String>();}
+    TxtFile(char*);
+    TxtFile(const String&);
+    TxtFile(TxtFile&);
+    TxtFile normalSort(); //yes
+    TxtFile reverseSort(); // speaks for itself
+    TxtFile notCaseSensitiveSort();	//speaks for itself
+    TxtFile humanReadableSort();	//sort "K M G" values
+    TxtFile numericSort();	//sort by numeric value not ascii
+    int getSize(){return lines.size();} // get the number of lines
+    void push(String&); // add a line to the object
+    Iterator end();
+    Iterator begin();
+};
+
+bool reverse(const String& lhs, const String& rhs ){
+    return lhs < rhs;
+}
+
+
+bool notCaseSensitive(const String& lhs, const String& rhs){
+    unsigned i = 0;
+    for (;i<lhs.length() && i<rhs.length(); i++){
+        if (tolower(lhs[i]) != tolower(rhs[i]))
+            return tolower(lhs[i]) < tolower(rhs[i]) ;
+
+    }
+    return i == lhs.length();
+};
+
+
+bool numeric(const String& lhs, const String& rhs){
+    int i = atoi(lhs.c_str());
+    int j = atoi(rhs.c_str());
+
+    return i<j;
+}
+
+bool hread(const String& lhs, const String& rhs){
+    char lhslast = lhs[lhs.length()-1];
+    char rhslast = rhs[rhs.length()-1];
+    int rhstrenght;
+    int lhstrenght;
+    switch(lhslast){
+        case 'B'  :
+            lhstrenght = 0;
+            break;
+        case 'K'  :
+            lhstrenght = 1;
+            break;
+        case 'M'  :
+            lhstrenght = 2;
+            break;
+        case 'G'  :
+            lhstrenght = 3;
+            break;
+        case 'T'  :
+            lhstrenght = 4;
+            break;
+        default:
+            lhstrenght = -1;
+    }
+    switch(rhslast){
+        case 'B'  :
+            rhstrenght = 0;
+            break;
+
+        case 'K'  :
+            rhstrenght = 1;
+            break;
+
+        case 'M'  :
+            rhstrenght = 2;
+            break;
+
+        case 'G'  :
+            rhstrenght = 3;
+            break;
+        case 'T'  :
+            rhstrenght = 4;
+            break;
+        default:
+            rhstrenght = -1;
+    }
+    if (lhstrenght < rhstrenght){
+        return true;
+    }else if (lhstrenght > rhstrenght){
+        return false;
+    }else{
+        return numeric(lhs, rhs);
+    }
+}
+
+TxtFile::TxtFile(char* filename){
+//    if (strcmp(filename, "") == 0)
+//        throw "Can't make object from nothing... -.-' Please specify something";
+//
+//    FILE* f = fopen(filename, "r");
+//    if (f == NULL) perror ("Error opening file");
+//
+//    while(!feof(f)){
+//        String line;
+//        for ( char c = char(); c == '\n'; c = fgetc(f)){
+//            line += c;
+//        }
+//        lines.push_back(line);
+//    }
+}
+
+TxtFile::TxtFile(const String& filename) {
+    if (filename == "")
+        throw "Can't make object from nothing... -.-' Please specify something";
+
+    FILE *f = fopen(filename.c_str(), "r");
+    if (f == NULL) perror("Error opening file");
+//    char c = (char) getc(f);
+//    String line = String(c);
+//    while(c != EOF && c != '\n'){
+//        c = (char) getc(f);
+//        line = line + c;
+    char *line = new char[1024];
+    String sLine;
+
+    while (line != NULL) {
+        line = fgets(line, 1024, f);
+        if (line != NULL){
+            sLine = String(line);
+            cout << line;
+            cout << sLine;
+        }
+    }
+    fclose(f);
+
+
+
+
+
+//        while (c != '\n') {
+//            line = line + c;
+//            cout<<"+";
+//
+//            c = (char) getc(f);
+//        }
+//
+//        lines.push_back(line);
+
+}
+
+TxtFile::TxtFile(TxtFile& other){
+    for (int i = 0; i<other.getSize(); i++){
+        lines.push_back(other.lines[i]);
+
+    }
+}
+
+void TxtFile::push(String& str){
+    lines.push_back(str);
+}
+
+
+TxtFile TxtFile::normalSort() {
+    TxtFile tmp;
+    for(unsigned i = 0; i < lines.size(); i++){
+        tmp.push(lines[i]);
+    }
+    Algorithms::sort(tmp.lines.begin(), tmp.lines.end());
+    return tmp;
+
+}
+
+TxtFile TxtFile::reverseSort(){
+    TxtFile tmp;
+    for (unsigned i = 0; i < lines.size(); i++)
+        tmp.push(lines[i]);
+    Algorithms::sort(tmp.begin(), tmp.end(), reverse);
+    return tmp;
+}
+
+TxtFile TxtFile::notCaseSensitiveSort(){
+    TxtFile tmp;
+
+    for (unsigned i = 0; i < lines.size(); i++)
+        tmp.push(lines[i]);
+
+    Algorithms::sort(tmp.begin(), tmp.end(), notCaseSensitive);
+
+
+    return tmp;
+}
+
+TxtFile TxtFile::numericSort(){
+    TxtFile tmp;
+
+    for (unsigned i = 0; i < lines.size(); i++)
+        tmp.push(lines[i]);
+
+    Algorithms::sort(tmp.begin(), tmp.end(), numeric);
+
+
+    return tmp;
+}
+
+
+TxtFile TxtFile::humanReadableSort(){
+    TxtFile tmp;
+
+    for (unsigned i = 0; i < lines.size(); i++)
+        tmp.push(lines[i]);
+
+    Algorithms::sort(tmp.begin(), tmp.end(), hread);
+
+
+    return tmp;
+}
+
+TxtFile::Iterator TxtFile::end(){
+    return lines.end();
+}
+
+TxtFile::Iterator TxtFile::begin(){
+    return lines.begin();
+}
+
+ostream &operator<<(ostream &os , TxtFile &rhs) {
+    TxtFile::Iterator it;
+    for (it = rhs.begin(); it != rhs.end(); it++){
+        os<<*it<<endl;
+    }
+
+    return os;
+}
+
+
+#endif //PROGHF_TXTFILE_H
